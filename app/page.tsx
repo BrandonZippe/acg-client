b11b2965ce557b09fd741109bbcdcf33488f1922
Brandon Zippe <btzippe@gmail.com>
2025-05-23 16:07:48 -0500

    mobile spacing

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="">
         <div>
           <Image
-            className=""
+            className="w-full"
             src="/ACGHeader.png"
             alt="Audio Collective Group"
             width={1543}
@@ -14,7 +14,7 @@ export default function Home() {
             priority
           />
         </div>
-        <section className="my-16 max-w-5xl mx-auto">
+        <section className="my-5 md:my-16 max-w-5xl mx-auto p-5">
           <h1 className="text-center my-5 text-4xl uppercase">About Us</h1>
           <p>Audio Collective Group is a family of passionate brands united by one mission: to empower the people who make music. As the parent company to a diverse range of musical recording equipment brands - from high-end boutique to budget-friendly - we believe that every musician, producer, engineer, and sound designer deserves access to tools built with integrity and quality. Whether it's in a world-class studio, a home setup, or on stage, our brands are behind the gear that makes musical moments possible.</p>
         </section>
@@ -56,7 +56,7 @@ export default function Home() {
           </div>
         </section>
         <section className="body-bg">
-          <div className="py-16 max-w-5xl mx-auto">
+          <div className="md:py-16 max-w-5xl mx-auto p-5">
             <h1 className="text-center my-5 text-4xl uppercase">Our Mission</h1>
             <p>At Audio Collective Group, our mission is to strengthen the music industry by supporting and scaling the brands that fuel it. We provide a shared backbone of operational excellence so that each brand can focus on what matters most: designing exceptional products for musicians, producers, engineers, and creators at every level. When we lift up the brands behind the music, we lift up the music itself.</p>
           </div>
