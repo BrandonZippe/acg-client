af0de6ec90bf0185019168e5eb37356e217c9d7a
Brandon Zippe <btzippe@gmail.com>
2026-01-13 15:47:27 -0600

    updates to rnr post

---
+++ app/rocknroller-multi-cart-announces-worldwide-licensing-agreement-with-audio-collective-group/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Header from "../header";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        <Header />
+        <section className="pr-wrap">
+          <h1 className="text-center my-5 text-4xl uppercase">RockNRoller® Multi-Cart® Announces WorldWide Licensing Agreement with Audio Collective Group</h1>
+          <p className="text-xl text-center">Audio Collective Group will now handle all sales, marketing, and logistics operations for the best-selling, transformable equipment utility cart brand.</p>
+          <p><strong>(Houston, TX -  March 18, 2024)</strong> <a href="https://rocknrollercart.com/">RockNRoller® Multi-Cart®</a>, the leading equipment utility cart brand with a patented 8-in-1 design, today announces a new worldwide licensing agreement with <a href="http://www.audiocollectivegroup.com">Audio Collective Group</a>, a parent company supporting and scaling premium music, audio, and professional equipment brands. Per the agreement, which went into effect February 2024 with RockNRoller Multi-Cart founder Gary-Michael Dahl, Audio Collective Group will oversee worldwide sales, marketing, and logistics operations for the RockNRoller® Multi-Cart® brand.</p>
+          <p>“Audio Collective Group is the right partner to take RockNRoller Multi-Cart to the next level in terms of global sales support, retail expansion, and operational scale,” said Gary-Michael Dahl, founder and inventor of RockNRoller Multi-Cart. “Their proven ability to grow brands worldwide—combined with deep experience in distribution, logistics, and channel marketing—creates new opportunities for our retail partners and distributors, and ultimately delivers a better experience for our customers.”</p>
+          <p>Audio Collective Group was formed to provide best-in-class infrastructure for established and emerging brands, offering shared expertise across worldwide sales, marketing strategy, fulfillment, customer support, and global logistics. Through this platform, ACG enables brands to expand their reach while maintaining the product quality, innovation, and brand identity that made them successful.</p>
+          <p>The patented RockNRoller® Multi-Cart® 8-in-1 design transforms into eight different configurations, replacing the need for multiple carts for different applications. A single cart can function as a standard hand truck, platform cart, wagon, hand cart, or dolly, and can also be used as a portable workstation or gear stand. Designed for efficiency, RockNRoller Multi-Carts can reduce the time and effort required to move gear or equipment by 50% or more. The current lineup includes 14 cart models along with a wide range of accessories such as shelf kits, solid decks, hang bags, and more, with additional accessories coming soon.</p>
+          <p>Renowned for their durability and adaptability—and even featured in major university engineering textbooks for outstanding design—RockNRoller® Multi-Carts® are trusted across a wide range of industries, including music, photography, DJ, audio/visual, broadcasting, construction, landscaping, IT services, office equipment supply, outdoor recreation, and beyond.</p>
+          <p>For more information on RockNRoller Multi-Cart, visit <a href="https://rocknrollercart.com/">https://rocknrollercart.com/</a>. For inquiries on sales or reseller opportunities, contact via <a href="mailto:sales@rocknrollercart.com">email</a>. </p>
+        </section>
+      </main>
+    </div>
+  );
+}
