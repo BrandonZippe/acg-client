2c2510a03961fd25850dbbee64e352190d67ca51
Brandon Zippe <btzippe@gmail.com>
2026-02-10 12:57:24 -0600

    adding phone number

--- app/footer.tsx
+++ app/footer.tsx
@@ -3,6 +3,7 @@ export default function Footer() {
   return (
       <footer className="">
         <p className="text-white text-center">© 2025 Audio Collective Group</p>
+        <p className="text-center text-md"><a href="tel:+15126471533" className="hover:underline">(512) 647-1533</a></p>
       </footer>
   );
 }
